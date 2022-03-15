413474a87180e8e88aea3154633c3b67704b2b6b
potemkinViktor <viktorapotemkin@gmail.com>
2022-03-15 13:55:32 +0300

    intial commit

---
+++ contracts/Arbitrage.sol
@@ -0,0 +1,53 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IERC20 {
+	function totalSupply() external view returns (uint);
+	function balanceOf(address account) external view returns (uint);
+	function transfer(address recipient, uint amount) external returns (bool);
+	function allowance(address owner, address spender) external view returns (uint);
+	function approve(address spender, uint amount) external returns (bool);
+	function transferFrom(address sender, address recipient, uint amount) external returns (bool);
+	event Transfer(address indexed from, address indexed to, uint value);
+	event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+interface IUniswapV2Router {
+  function getAmountsOut(uint256 amountIn, address[] memory path) external view returns (uint256[] memory amounts);
+  function swapExactTokensForTokens(uint256 amountIn, uint256 amountOutMin, address[] calldata path, address to, uint256 deadline) external returns (uint256[] memory amounts);
+}
+
+interface IUniswapV2Pair {
+  function token0() external view returns (address);
+  function token1() external view returns (address);
+  function swap(uint256 amount0Out,	uint256 amount1Out,	address to,	bytes calldata data) external;
+}
+
+contract Arbitrage is Ownable {
+    function swap(address router, address _tokenIn, address _tokenOut, uint256 _amount) private {
+        IERC20(_tokenIn).approve(router, _amount);
+        address[] memory path;
+        path = new address[](2);
+        path[0] = _tokenIn;
+        path[1] = _tokenOut;
+        uint deadline = block.timestamp + 300;
+        IUniswapV2Router(router).swapExactTokensForTokens(_amount, 1, path, address(this), deadline);
+    }
+
+    function getAmountOutMin(address router, address _tokenIn, address _tokenOut, uint256 _amount) public view returns (uint256) {
+		address[] memory path;
+		path = new address[](2);
+		path[0] = _tokenIn;
+		path[1] = _tokenOut;
+		uint256[] memory amountOutMins = IUniswapV2Router(router).getAmountsOut(_amount, path);
+		return amountOutMins[path.length - 1];
+	}
+
+    function estimateDualDexTrade(address _router1, address _router2, address _token1, address _token2, uint256 _amount) external view returns (uint256) {
+        uint256 amtBack1 = getAmountOutMin(_router1, _token1, _token2, _amount);
+		uint256 amtBack2 = getAmountOutMin(_router2, _token2, _token1, amtBack1);
+		return amtBack2;
+    }
+}
